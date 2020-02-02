2c078b69a80d8b9cd4abcf5b88bc9ea16a628ca1
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-02-02 09:55:55 -0500

    init

--- risr3d_eq/config.m
+++ risr3d_eq/config.m
@@ -6,13 +6,13 @@ p.simID = 'risr3d';
 p.nml = [cwd,'/config.nml'];
 p.xdist = 100e3;    %eastward distance
 p.ydist = 600e3;    %northward distance
-p.lxp=10;
-p.lyp=20;
-p.glat=74.73;
-p.glon=265.095; % 94.905 W
-p.I=90;
-p.nmf=5e11;
-p.nme=2e11;
+p.lxp = 10;
+p.lyp = 20;
+p.glat = 74.73;
+p.glon = 265.095; % 94.905 W
+p.I = 90;
+p.nmf = 5e11;
+p.nme = 2e11;
 
 % [f107a, f107, ap] = activ;
 % import geomagindices as gi
