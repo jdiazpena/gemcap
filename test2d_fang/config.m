0a52d63f9fcc62d08083b595f0095b43e894473e
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-02-15 23:06:44 -0500

    matlab: naming of efield and precip disturbance

--- test2d_fang/config.m
+++ test2d_fang/config.m
@@ -18,8 +18,10 @@ p.glon = 212.95;
 p.I = 90;
 p.nmf = 5e11;
 p.nme = 2e11;
-p.fracwidth = 1/7;
+p.Efield_fracwidth = 1/7;
 p.Etarg = 50e-3;  % [V/m]
+p.precip_latwidth = 1/4;
+p.precip_lonwidth = 1/4;
 
 p = merge_struct(p, read_config(p.nml));
 % import geomagindices as gi
