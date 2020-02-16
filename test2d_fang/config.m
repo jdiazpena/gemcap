c11760a3c8569127622ec68a4cd657029222c172
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-02-15 22:35:30 -0500

    update params

--- test2d_fang/config.m
+++ test2d_fang/config.m
@@ -1,12 +1,14 @@
 function config()
 %% 2D east-west test
-%
-cwd = fileparts(mfilename('fullpath'));
+
+gemroot = [pwd, '/../../gemini'];
+addpath([gemroot,'/script_utils'], [gemroot,'/setup'])
 
 p.format = 'hdf5';
-p.nml = [cwd,'/config.nml'];
-p.eqdir = [cwd, '/../../gemini_sim/test2d_eq'];
-p.simdir = [cwd, '/../../gemini_sim/test2d_fang'];
+p.nml = [pwd,'/config.nml'];
+p.eqdir = [pwd, '/../../gemini_sim/test2d_eq'];
+p.eqnml = [p.eqdir, '/inputs/config.nml'];
+p.simdir = [pwd, '/../../gemini_sim/test2d_fang'];
 p.xdist = 200e3;    %eastward distance
 p.ydist = 600e3;    %northward distance
 p.lxp = 80;
@@ -16,11 +18,13 @@ p.glon = 212.95;
 p.I = 90;
 p.nmf = 5e11;
 p.nme = 2e11;
+p.fracwidth = 1/7;
+p.Etarg = 50e-3;  % [V/m]
 
 p = merge_struct(p, read_config(p.nml));
 % import geomagindices as gi
 % gi.get_indices('2013-02-20T05', smoothdays=81)
 p.activ = [108.9, 111.0, 5];
 
-model_setup_equilibrium(p)
+model_setup_interp(p)
 end % function
