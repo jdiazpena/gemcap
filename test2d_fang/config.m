35995fb22e03044cd6f28a137ed30c41a50c251b
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-02-19 14:12:09 -0500

    test2d_fang: new matlab api

--- test2d_fang/config.m
+++ test2d_fang/config.m
@@ -1,28 +1,32 @@
 function config()
 %% 2D east-west test
+%
+
+%% boilerplate for each config.m file
 cwd = fileparts(mfilename('fullpath'));
-gemroot = [cwd, '/../../gemini'];
+gemroot = getenv('GEMINI_ROOT');
 addpath([gemroot,'/script_utils'], [gemroot,'/setup'])
+
+% convenience
+simroot = [cwd, '/../../gemini_sim'];
 
 p.format = 'h5';
 p.nml = [cwd,'/config.nml'];
-p.eqdir = [cwd, '/../../gemini_sim/test2d_eq'];
+p.eqdir = [simroot, '/test2d_eq'];
 p.eqnml = [p.eqdir, '/inputs/config.nml'];
-p.simdir = [pwd, '/../../gemini_sim/test2d_fang'];
-p.xdist = 200e3;    %eastward distance
-p.ydist = 1000e3;    %northward distance
-p.lxp = 40;
-p.lyp = 1;
-p.glat = 67.11;
-p.glon = 212.95;
-p.I = 90;
+p.simdir = [simroot, '/test2d_fang'];
 
-p.Efield_fracwidth = 1/7;
+p = merge_struct(p, read_nml(p.nml));
+
+%% perturbations
 p.Etarg = 50e-3;  % [V/m]
+
+% normalized widths
+p.Efield_fracwidth = 1/7;
 p.precip_latwidth = 1/4;
 p.precip_lonwidth = 1/4;
 
-p = merge_struct(p, read_config(p.nml));
-
+%% setup simulation
 model_setup_interp(p)
+
 end % function
