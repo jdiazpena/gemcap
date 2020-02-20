03bb20c71fe271368eb8b13176dcb31878e0f39b
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-02-20 17:44:17 -0500

    risr2d_fang add needed param

--- risr2d_fang/config.m
+++ risr2d_fang/config.m
@@ -10,10 +10,15 @@ p.nml = [cwd,'/config.nml'];
 p = merge_struct(p, read_nml(p.nml));
 p.simdir = absolute_path(['../', fileparts(p.indat_size)]);
 
+p.eqdir = absolute_path([p.simdir, '/../risr2d_eq']);
+p.eqnml = [p.eqdir, '/inputs/config.nml'];
+
 p.format = 'h5';
 
-%% perturbation
+%% perturbations
+p.Etarg = 50e-3;  % [V/m]
 
+% normalized widths
 p.fracwidth = 1/7;
 
 %% setup simulation
