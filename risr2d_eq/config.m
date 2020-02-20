e76e728449dbc20bae6b6798715915cae94d0d4d
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-02-20 17:42:40 -0500

    create risr2d_fang north-south

---
+++ risr2d_eq/config.m
@@ -0,0 +1,18 @@
+function config()
+%% 2D north-south equilibrium
+%
+
+%% boilerplate for each config.m file
+cwd = fileparts(mfilename('fullpath'));
+gemroot = getenv('GEMINI_ROOT');
+addpath([gemroot, '/script_utils'], [gemroot,'/setup'])
+p.nml = [cwd,'/config.nml'];
+p = merge_struct(p, read_nml(p.nml));
+p.simdir = absolute_path(['../', fileparts(p.indat_size)]);
+
+p.format = 'h5';
+
+%% setup simulation
+model_setup_equilibrium(p)
+
+end % function
