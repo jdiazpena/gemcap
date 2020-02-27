1cb580fdf246833f82703324b960638904d676f5
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2020-02-27 13:47:02 -0500

    more params in config.nml

--- jgr01_fang/config.m
+++ jgr01_fang/config.m
@@ -6,12 +6,9 @@ function config()
 cwd = fileparts(mfilename('fullpath'));
 gemroot = getenv('GEMINI_ROOT');
 addpath([gemroot, '/script_utils'], [gemroot,'/setup'])
-p.nml = [cwd,'/config.nml'];
-p = merge_struct(p, read_nml(p.nml));
-p.simdir = absolute_path(['../', fileparts(p.indat_size)]);
 
-p.eqdir = absolute_path([p.simdir, '/../jgr01_eq']);
-p.eqnml = [p.eqdir, '/inputs/config.nml'];
+p = read_nml(cwd);
+p.simdir = absolute_path(['../', fileparts(p.indat_size)]);
 
 p.format = 'h5';
 p.realbits = 64;
