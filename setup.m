5376b0d2d13381a1fe1bd6dba5c816f78a88fae7
Michael Hirsch <scivision@users.noreply.github.com>
2020-09-25 10:24:42 -0400

    setup.m modernize

--- setup.m
+++ setup.m
@@ -1,14 +1,14 @@
 function setup()
 %% configure paths to work with MatGemini
-narginchk(0,0)
+
 cwd = fileparts(mfilename('fullpath'));
-gemini_matlab = fullfile(cwd, '../mat_gemini');
+gemini_matlab = fullfile(cwd, "../mat_gemini");
 if ~isfolder(gemini_matlab)
-  cmd = ['git -C ', fullfile(cwd, '..'), ' clone https://github.com/gemini3d/mat_gemini'];
+  cmd = "git -C " + fullfile(cwd, "..") + " clone https://github.com/gemini3d/mat_gemini.git";
   disp(cmd)
   ret = system(cmd);
   assert(ret==0, 'problem downloading Gemini Matlab functions')
 end
-run(fullfile(gemini_matlab, 'setup.m'))
+run(fullfile(gemini_matlab, "setup.m"))
 
 end
